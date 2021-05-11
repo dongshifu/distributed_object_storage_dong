f2bced809d5f7392909e79bb19a9b3bc0c10e68c
dongshifu <1768718795@qq.com>
2021-05-11 20:47:10 +0800

    调整结构

--- v6/test/putmapping.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl localhost:9200/metadata -XDELETE
-
-curl localhost:9200/metadata -XPUT -d'{"mappings":{"objects":{"properties":{"name":{"type":"string","index":"not_analyzed"},"version":{"type":"integer"},"size":{"type":"integer"},"hash":{"type":"string"}}}}}'
